8126c0a733b1ed43d014d1d0ddce58c5593c20d2
RylanRRRB <22109498@student.hhs.nl>
2022-12-22 15:07:54 +0100

    Fix buttons

--- src/main/java/com/example/demo2/HelloController.java
+++ src/main/java/com/example/demo2/HelloController.java
@@ -4,28 +4,93 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.text.Text;
 
 
 public class HelloController {
+    public ToggleButton PrivZichtbaarheidButton;
+    public ToggleButton PubZichtbaarheidButton;
     @FXML
-    private Button kurkuma_min;
+    private Button categorieEenMinButton;
     @FXML
-    private Button kurkuma_plus;
+    private Button categorieEenPlusButton;
     @FXML
-    private Label kurkuma_text;
-
-    double i=0.0;
+    private Label categorieEenGramText;
 
 
+    @FXML
+    private Button categorieTweeMinButton;
+    @FXML
+    private Button categorieTweePlusButton;
+    @FXML
+    private Text categorieTweeGramText;
 
-    public void kurkuma_min(ActionEvent actionEvent) {
-        i-=0.5;
-        kurkuma_text.setText(String.valueOf(i));
+
+    @FXML
+    private Button categorieDriePlusButton;
+    @FXML
+    private Button categorieDrieMinButton;
+    @FXML
+    private Text categorieDrieGramText;
+
+
+    double i = 0.0;
+    double i2 = 0.0;
+    boolean isReceptPubliek = false;
+
+
+
+
+    public void categorieEenMinButtonAction(ActionEvent actionEvent) {
+        if (i > 0.0) {
+            categorieEenMinButton.setDisable(false);
+            i -= 0.5;
+            categorieEenGramText.setText(String.valueOf(i));
+        }
     }
 
-    public void kurkuma_(ActionEvent actionEvent) {
-        i+=0.5;
-        kurkuma_text.setText(String.valueOf(i));
+    public void categorieEenPlusButtonAction(ActionEvent actionEvent) {
+        i += 0.5;
+        categorieEenGramText.setText(String.valueOf(i));
+    }
+
+    public void categorieTweeMinButtonAction(ActionEvent actionEvent) {
+        if (i2 > 0.0) {
+            categorieTweeMinButton.setDisable(false);
+            i2 -= 0.5;
+            categorieTweeGramText.setText(String.valueOf(i2));
+        }
+    }
+
+    public void categorieTweePlusButtonAction(ActionEvent actionEvent) {
+        i2 += 0.5;
+        categorieTweeGramText.setText(String.valueOf(i2));
+    }
+
+    public void categorieDrieMinButtonAction(ActionEvent actionEvent) {
+        if (i2 > 0.0) {
+            categorieDrieMinButton.setDisable(false);
+            i2 -= 0.5;
+            categorieDrieGramText.setText(String.valueOf(i2));
+        }
+    }
+    public void categorieDriePlusButtonAction(ActionEvent actionEvent) {
+        i2 += 0.5;
+        categorieDrieGramText.setText(String.valueOf(i2));
+    }
+
+    public void PubZichtbaarheidButtonAction(ActionEvent actionEvent) {
+        PrivZichtbaarheidButton.setSelected(false);
+        isReceptPubliek = true;
+        PubZichtbaarheidButton.setStyle("-fx-background-color:gray");
+        PrivZichtbaarheidButton.setStyle("-fx-background-color:red");
+    }
+
+    public void PrivZichtbaarheidButtonAction(ActionEvent actionEvent) {
+        PubZichtbaarheidButton.setSelected(false);
+        isReceptPubliek = false;
+        PubZichtbaarheidButton.setStyle("-fx-background-color:red");
+        PrivZichtbaarheidButton.setStyle("-fx-background-color:gray");
     }
 }
